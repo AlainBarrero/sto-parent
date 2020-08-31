dfe44b1f88e99efdd3e45ef8326518e6803d9079
alainbarrero <abarrero@indaba.es>
2020-08-31 14:06:40 +0200

    swagger codegen

--- stock-user-parent/stock-user-impl/src/main/java/com/stock/user/impl/config/UserImplConfig.java
+++ stock-user-parent/stock-user-impl/src/main/java/com/stock/user/impl/config/UserImplConfig.java
@@ -12,22 +12,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.stock.user.impl.internal.model.EntityMarker;
 import com.stock.user.impl.internal.repository.RepositoryMarker;
 import com.stock.user.impl.service.ApiImplMarker;
 
 @Configuration
-@EnableTransactionManagement
 @EntityScan(basePackageClasses = EntityMarker.class)
 @EnableJpaRepositories(basePackageClasses = RepositoryMarker.class)
 @ComponentScan(basePackageClasses = {ApiImplMarker.class})
@@ -45,9 +40,7 @@ public class UserImplConfig {
         entityManagerFactory.setDataSource(dataSource());
         entityManagerFactory.setJpaVendorAdapter(jpaVendorAdapter());
         entityManagerFactory.setPackagesToScan(EntityMarker.class.getPackage().getName());
-        entityManagerFactory.setPersistenceUnitName("default");
         entityManagerFactory.setJpaProperties(hibernateProperties());
-        entityManagerFactory.afterPropertiesSet();
 
         return entityManagerFactory.getObject();
     }
@@ -60,27 +53,15 @@ public class UserImplConfig {
     }
 
     @Bean
-    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(entityManagerFactory);
-
-        return transactionManager;
-    }
-
-    @Bean
-    public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
-        return new PersistenceExceptionTranslationPostProcessor();
-    }
-
-    @Bean
-    public JpaVendorAdapter jpaVendorAdapter() {
-        return new HibernateJpaVendorAdapter();
-    }
-
-    @Bean
     @Primary
     @ConfigurationProperties(prefix = "spring.jpa")
     public Properties hibernateProperties() {
         return new Properties();
     }
+    
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        return new HibernateJpaVendorAdapter();
+    }
+
 }
